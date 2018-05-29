f03b9eeb5d844901521916db6f6331a58132c2aa
yh <zjuyh@sina.cn>
2018-05-29 14:58:10 +0800

    naiver

--- src/mapCompare.cpp
+++ src/mapCompare.cpp
@@ -47,7 +47,10 @@ public:
     DP mapCloud0;
     DP mapCloud1;
 
+    shared_ptr<NNS> distanceNNS;
+
     void process();
+    vector<int> intersection(vector<int> a, vector<int> b);
 
 };
 
@@ -75,10 +78,74 @@ mapCompare::mapCompare(ros::NodeHandle& n):
 
 void mapCompare::process()
 {
+//    int rowLineSession = mapCloud0.getDescriptorStartingRow("session");
+    int rowLineSalient = mapCloud0.getDescriptorStartingRow("salient");
 
 
+    // save and anal
+    vector<int> salientIndex0;
+    DP salientCloud0 = mapCloud0.createSimilarEmpty();
+    int cnt0=0;
+    for(int i=0; i<mapCloud0.features.cols(); i++)
+    {
+        if(mapCloud0.descriptors(rowLineSalient, i) == 1)
+        {
+            salientCloud0.setColFrom(cnt0, mapCloud0, i);
+            salientIndex0.push_back(i);
+            cnt0++;
+        }
+    }
+    salientCloud0.conservativeResize(cnt0);
 
+    vector<int> salientIndex1;
+    DP salientCloud1 = mapCloud1.createSimilarEmpty();
+    int cnt1=0;
+    for(int i=0; i<mapCloud1.features.cols(); i++)
+    {
+        if(mapCloud1.descriptors(rowLineSalient, i) == 1)
+        {
+            salientCloud1.setColFrom(cnt1, mapCloud1, i);
+            salientIndex1.push_back(i);
+            cnt1++;
+        }
+    }
+    salientCloud1.conservativeResize(cnt1);
 
+    //Compare, the num of same index and mean distance
+
+    vector<int> samer = this->intersection(salientIndex0, salientIndex1);
+    cout<<"Same Index:  "<<samer.size()<<endl;
+
+    distanceNNS.reset(NNS::create(salientCloud0.features, salientCloud0.features.rows()-1, NNS::KDTREE_LINEAR_HEAP, NNS::TOUCH_STATISTICS));
+    PM::Matches matches_NNS(
+        Matches::Dists(1, salientCloud1.features.cols()),
+        Matches::Ids(1, salientCloud1.features.cols())
+    );
+    distanceNNS->knn(salientCloud1.features, matches_NNS.ids, matches_NNS.dists, 1, 0);
+
+    vector<double> distsVector;
+    for(int i=0; i<salientCloud1.features.cols(); i++)
+    {
+        distsVector.push_back(sqrt(matches_NNS.dists(0, i)));
+    }
+
+    double meanDist = std::accumulate(std::begin(distsVector), std::end(distsVector), 0.0) / distsVector.size();
+    cout<<"Mean Distance:   "<<meanDist<<endl;
+
+}
+
+vector<int> mapCompare::intersection(vector<int> a, vector<int> b)
+{
+    vector<int> resultVector(0);
+
+    for(int bb=0; bb<b.size(); bb++)
+    {
+        vector<int>::iterator result = std::find(a.begin(), a.end(), b.at(bb));
+        if(result != a.end())
+            resultVector.push_back(*result);
+    }
+
+    return resultVector;
 }
 
 int main(int argc, char **argv)
