b639346e0484e7ad64b6a3202b4f84491c81da34
Huan Yin <zjuyinhuan@gmail.com>
2018-07-04 11:58:43 +1000

    go out for lunch, confised

--- gurobi/test.m
+++ gurobi/test.m
@@ -1,4 +1,4 @@
-function [ ] = test( )
+function [result ] = test( )
 %TEST Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -6,8 +6,23 @@ function [ ] = test( )
 
 %     allZeros = zeros(6000, 500000);
 
-a = sparse(round(rand(60,500)));
-    
+disp('Generating')
+model.A = sparse(round(rand(1000,100000)));
+disp('Setting')
+model.obj = ones(1,100000);    
+model.rhs = 2*ones(1,1000);
+model.sense = '>';
+model.vtype = 'B';
+model.modelsense = 'min';
+
+gurobi_write(model, 'test.lp');
+
+params.outputflag = 0;
+
+result = gurobi(model, params);
+
+dlmwrite('result.txt', result.x);
+
 end
 
  
