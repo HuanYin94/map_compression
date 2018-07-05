edf8e39c31fd157abffa47ea6f9797eed1a31649
yh@uts <zjuyinhuan@gmail.com>
2018-07-05 16:58:06 +1000

    salient prediction num count

---
+++ gurobi/salientNumCnt.m
@@ -0,0 +1,28 @@
+function [  ] = salientNumCnt( filesDir, totalNum )
+%SALIENTNUMCNT Summary of this function goes here
+%   Detailed explanation goes here
+    
+    allPoints = zeros(1,totalNum);
+
+    fileExt = '*.txt';
+    
+    files = dir(fullfile(filesDir,fileExt));  
+        
+    for i=0:length(files)-1
+        i
+        fileCnt = num2str(i);
+        fileName = [filesDir, fileCnt, '.txt'];
+        
+        file_t = fopen(fileName);
+        pointID = fscanf(file_t, '%d');
+        fclose(file_t);
+    
+        allPoints(sub2ind(size(allPoints), ones(1, length(pointID)), (pointID+1)')) = 1;
+
+    end
+    
+    disp('salient predicted:');
+    disp(sum(allPoints));
+        
+end
+
