e38b8ace77f5dd9cf9606c272b0f621efb38dbce
Huan Yin <zjuyinhuan@gmail.com>
2018-07-21 14:36:54 +1000

    foramt & converting

--- prepare/convertPLY.m
+++ prepare/convertPLY.m
@@ -2,12 +2,25 @@ function [  ] = convertPLY( openDir, saveDir )
 %CONVERTPLY Summary of this function goes here
 %   Detailed explanation goes here
     
+    % For KITTI Dataset
+
     fileExt = '*.bin';
     files = dir(fullfile(openDir,fileExt));  
         
     for i=0:length(files)-1
+        i
         
+        fileID = sprintf('%06d', i);
+        fileName = [openDir, fileID, '.bin'cd ];
         
+        xyz = readVelodyne(fileName);
+        xyz = xyz';
+        xyz = xyz(:,1:3);
+        
+        pt = pointCloud(xyz);
+        
+        saveName = [saveDir, num2str(i), '.ply'];
+        pcwrite(pt, saveName);
         
     end
 
