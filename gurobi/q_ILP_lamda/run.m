f96c92e39b2fa267d835805cb70ab282b589def8
Huan Yin <zjuyinhuan@gmail.com>
2018-08-07 20:00:01 +1000

    need re-run?

--- gurobi/q_ILP_lamda/run.m
+++ gurobi/q_ILP_lamda/run.m
@@ -6,7 +6,7 @@
 
 totalNum = 1866088;
 
-loopCompress(0.1, '/home/yh/mapModel/2018/08.03/weightVector.txt', '/home/yh/mapModel/2018/08.03/visMatrix/', 50, totalNum, 1600, '/home/yh/mapModel/2018/08.03/gurobi_compress_0.2/');
+loopCompress(0.1, '/home/yh/mapModel/2018/08.03/weightVector.txt', '/home/yh/mapModel/2018/08.03/visMatrix/', 50, totalNum, 1550, '/home/yh/mapModel/2018/08.03/gurobi_compress_0.2/');
 
 ratio = salientNumCnt( '/home/yh/mapModel/2018/08.03/gurobi_compress_0.2/', totalNum ) / totalNum
 
