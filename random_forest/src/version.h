78b87cf7b48f2ab5e18e95c94e5b7f8cc0587234
Huan Yin <zjuyinhuan@gmail.com>
2018-08-07 19:08:33 +1000

    I get the faster.

---
+++ random_forest/src/version.h
@@ -0,0 +1,3 @@
+#ifndef RANGER_VERSION
+#define RANGER_VERSION "0.10.2"
+#endif
--- src/generateAllFeatures.cpp
+++ src/generateAllFeatures.cpp
@@ -220,6 +220,8 @@ void Generation::process()
     //  1    2    3      4       5      6        7           8       9      10     11
     // n_x  n_y  n_z  lamda1  lamda2  lamda3  Densitiy  Intenisty  Range  Height  LABEL
     ofstream saver(saveFeaturesName);
+    saver<<"n_x"<<"   "<<"n_y"<<"   "<<"n_z"<<"   "<<"lamda1"<<"   "<<"lamda2"<<"   "
+            <<"lamda3"<<"   "<<"Densitiy"<<"    "<<"Intenisty"<<"   "<<"Range"<<"   "<<"Height"<<"  "<<"LABEL"<<endl;
     for(int m=0; m<mapCloud.features.cols(); m++)
     {
         // the order of lamdas, fomr small to lasrge
