6b036e0658400c87365f7f6413d3d471110c7903
Huan Yin <zjuyinhuan@gmail.com>
2018-08-07 19:45:34 +1000

    run on laptop

---
+++ evaluation/predict_effe.m
@@ -0,0 +1,23 @@
+function [  ] = predict_effe( testData, predictData, threshold )
+%PREDICT_EFFE Summary of this function goes here
+%   Detailed explanation goes here
+    
+    % loading manually
+
+    % for test file, the lastcolumn is the ground truth
+    gt_index = find(testData == 1);
+    
+    % the first column represtns the ZERO probability
+    pr_index = find(predictData < threshold);
+    
+    sameNum = intersect(pr_index, gt_index);
+    
+    disp(length(gt_index))
+    
+    disp(length(pr_index))
+    
+    prediction = length(sameNum) / length(pr_index)
+    recall = length(sameNum) / length(gt_index)
+    
+end
+
--- gurobi/q_ILP_lamda/run.m
+++ gurobi/q_ILP_lamda/run.m
@@ -1,19 +1,13 @@
 %% YQ Dataset
 
+% same device as the random forest, no server, we use
+
 % Laptop
 
-% totalNum = 2769950;
-%  loopCompress(0.1, '/home/yh/mapModel/2018/08.03/weightVector.txt', '/home/yh/mapModel/2018/08.03/visMatrix/', 100, totalNum, 300, '/home/yh/mapModel/2018/08.03/gurobi_compress_0.05/');
-% % loopCompress(0.1, '/home/yh/mapModel/2018/08.03/weightVector.txt', '/home/yh/mapModel/2018/08.03/visMatrix/', 50, 500000, 350, '/home/yh/mapModel/2018/08.03/gurobi_compress_0.1/');
-% % loopCompress(0.1, '/home/yh/mapModel/2018/08.03/weightVector.txt', '/home/yh/mapModel/2018/08.03/visMatrix/', 50, 500000, 350, '/home/yh/mapModel/2018/08.03/gurobi_compress_0.2/');
-% 
-% ratio = salientNumCnt( '/home/yh/mapModel/2018/08.03/gurobi_compress_0.05/', totalNum ) / totalNum
-
-% server
-
 totalNum = 1866088;
-loopCompress(0.1, '/home/huyin/weightVector.txt', '/home/huyin/visMatrix/', 50, totalNum, 520, '/home/huyin/gurobi_compress/');
 
-ratio = salientNumCnt( '/home/huyin/gurobi_compress/', totalNum ) / totalNum
+loopCompress(0.1, '/home/yh/mapModel/2018/08.03/weightVector.txt', '/home/yh/mapModel/2018/08.03/visMatrix/', 50, totalNum, 1600, '/home/yh/mapModel/2018/08.03/gurobi_compress_0.2/');
+
+ratio = salientNumCnt( '/home/yh/mapModel/2018/08.03/gurobi_compress_0.2/', totalNum ) / totalNum
 
 %% KITTI Dataset
