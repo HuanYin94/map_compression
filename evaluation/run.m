2e21738d493e063a9708819d66bfa5d6ce0e76ec
Huan Yin <zjuyinhuan@gmail.com>
2018-08-08 21:28:59 +1000

    on the bus

--- evaluation/run.m
+++ evaluation/run.m
@@ -1,1 +1,8 @@
-predict_effe(test, rangerout, 0.58, '/home/yh/mapModel/2018/08.03/learn/test.txt');
+%% kitti
+predict_effe(test, predict, 0.6568, '/home/yh/mapModel/2018/08.02/learn/predict_label_0.1.txt');  % diff 4 points
+
+%% yq
+
+
+
+
