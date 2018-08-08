687c430b70337b433627ab3008bea087f3f9586f
Huan Yin <zjuyinhuan@gmail.com>
2018-08-08 10:34:38 +1000

    FUCK filter, 5*5*5=125

--- gurobi/q_ILP_lamda/run.m
+++ gurobi/q_ILP_lamda/run.m
@@ -4,10 +4,11 @@
 
 % Laptop
 
-totalNum = 1866088;
-
-loopCompress(0.1, '/home/yh/mapModel/2018/08.03/weightVector.txt', '/home/yh/mapModel/2018/08.03/visMatrix/', 50, totalNum, 520, '/home/yh/mapModel/2018/08.03/gurobi_compress_0.05/');
-
-ratio = salientNumCnt( '/home/yh/mapModel/2018/08.03/gurobi_compress_0.05/', totalNum ) / totalNum
+% totalNum = 1866088;
+% 
+% loopCompress(0.1, '/home/yh/mapModel/2018/08.03/weightVector.txt', '/home/yh/mapModel/2018/08.03/visMatrix/', 50, totalNum, 520, '/home/yh/mapModel/2018/08.03/gurobi_compress_0.05/');
+% 
+% ratio = salientNumCnt( '/home/yh/mapModel/2018/08.03/gurobi_compress_0.05/', totalNum ) / totalNum
 
 %% KITTI Dataset
+
--- prepare/run.m
+++ prepare/run.m
@@ -1,6 +1,6 @@
 
-meanDis09 = getMeanDistance_kitti(kitti09);
-ForKITTIPoses( '/home/yh/mapModel/2018/08.02/kitti09_pose.txt', '/home/yh/mapModel/2018/08.02/kitti09_keep.txt', kitti09, meanDis09);
+meanDis10 = getMeanDistance_kitti(kitti10);
+ForKITTIPoses( '/home/yh/mapModel/2018/08.02/kitti10_pose.txt', '/home/yh/mapModel/2018/08.02/kitti10_keep.txt', kitti10, meanDis10);
 
 % meanDis0301 = getMeanDistance_yq(icp0301);
 % poseSparsification( icp0301, meanDis0301, '/home/yh/mapModel/2018/08.03/keep_0301.txt');
