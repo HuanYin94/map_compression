635db7271e2016b92ae9f5140aa795b79e04019a
Huan Yin <zjuyinhuan@gmail.com>
2018-08-09 10:08:26 +1000

    start a new day

--- evaluation/run.m
+++ evaluation/run.m
@@ -1,7 +1,8 @@
 %% kitti
-predict_effe(test, predict, 0.6568, '/home/yh/mapModel/2018/08.02/learn/predict_label_0.1.txt');  % diff 4 points
+predict_effe(test, predict, 0.659, '/home/yh/mapModel/2018/08.02/learn/predict_label_0.1.txt');  % diff 4 points
 
 %% yq
+
 
 
 
