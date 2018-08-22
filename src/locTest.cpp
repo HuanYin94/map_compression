fb7d37c1dca66fa449ca562355f10983a5457590
Huan Yin <zjuyinhuan@gmail.com>
2018-08-22 19:52:30 +1000

    pose tracking test :blush:

--- src/locTest.cpp
+++ src/locTest.cpp
@@ -28,28 +28,32 @@ class locTest
 {
     typedef PointMatcher<float> PM;
     typedef PM::DataPoints DP;
-    typedef PM::Matches Matches;
 
 public:
     locTest(ros::NodeHandle &n);
     ~locTest();
     ros::NodeHandle& n;
 
-    string loadMapName;
-    string loadTrajName;
-    string icpYaml;
-    string loadVeloDir;
+    string wholeMapName;
+    string icpFileName;
+    string velodyneDirName;
+    string keepIndexName;
+
     string saveLocName;
     string saveTimeName;
-    string inputYaml;
+
+    string icpYaml;
+    string inputFilterYaml;
 
     DP mapCloud;
+    DP velodyneCloud;
 
-    int mapLength;
+    double deltaTime;
+
     int startIndex;
     int endIndex;
 
-    vector<vector<double>> initPoses;
+    bool isKITTI;
 
     //icp
     PM::DataPointsFilters inputFilters;
@@ -57,13 +61,16 @@ public:
     PM::TransformationParameters Ticp;
     PM::TransformationParameters Tinit;
 
-    Eigen::Vector2d gnd;
-    Eigen::Vector2d loc;
-
-    double deltaTime;
+    vector<vector<double>> initPoses;
+    vector<int> indexVector;
 
     void process(int index);
-    double getLocError(Eigen::Vector2d gnd, Eigen::Vector2d loc);
+    DP readYQBin(string filename);
+    DP readKITTIBin(string filename);
+
+    ros::Publisher mapCloudPub;
+    ros::Publisher velCloudPub;
+    tf::TransformBroadcaster tfBroadcaster;
 
 };
 
@@ -72,32 +79,33 @@ locTest::~locTest()
 
 locTest::locTest(ros::NodeHandle& n):
     n(n),
-    loadMapName(getParam<string>("loadMapName", ".")),
-    loadTrajName(getParam<string>("loadTrajName", ".")),
-    mapLength(getParam<int>("mapLength", 0)),
+    wholeMapName(getParam<string>("wholeMapName", ".")),
+    icpFileName(getParam<string>("icpFileName", ".")),
     startIndex(getParam<int>("startIndex", 0)),
     endIndex(getParam<int>("endIndex", 0)),
     icpYaml(getParam<string>("icpYaml", ".")),
-    loadVeloDir(getParam<string>("loadVeloDir", ".")),
+    velodyneDirName(getParam<string>("velodyneDirName", ".")),
     saveLocName(getParam<string>("saveLocName", ".")),
     saveTimeName(getParam<string>("saveTimeName", ".")),
-    inputYaml(getParam<string>("inputYaml", "."))
+    inputFilterYaml(getParam<string>("inputFilterYaml", ".")),
+    isKITTI(getParam<bool>("isKITTI", ".")),
+    keepIndexName(getParam<string>("keepIndexName", "."))
 {
+    mapCloudPub = n.advertise<sensor_msgs::PointCloud2>("map_cloud", 2, true);
+    velCloudPub = n.advertise<sensor_msgs::PointCloud2>("velodyne_cloud", 2, true);
 
     // load
-    mapCloud = DP::load(loadMapName);
-    ifstream inputiffilter(inputYaml);
-    inputFilters = PM::DataPointsFilters(inputiffilter);
+    mapCloud = DP::load(wholeMapName);
 
     // read initial transformation
     int x, y;
     double temp;
     vector<double> test;
-    ifstream in(loadTrajName);
+    ifstream in(icpFileName);
     if (!in) {
         cout << "Cannot open file.\n";
     }
-    for (y = 0; y < mapLength; y++) {
+    for (y = 0; y < 99999; y++) {
         test.clear();
     for (x = 0; x < 16; x++) {
       in >> temp;
@@ -106,6 +114,19 @@ locTest::locTest(ros::NodeHandle& n):
       initPoses.push_back(test);
     }
     in.close();
+
+    // read all the effective index from list in the txt
+    int l;
+    ifstream in_(keepIndexName);
+    if (!in_) {
+        cout << "Cannot open file.\n";
+    }
+    while(!in_.eof())
+    {
+        in_>>l;
+        indexVector.push_back(l);
+    }
+    indexVector.pop_back();  // last one repeated
 
     cout<<"start locTest"<<endl;
 
@@ -119,10 +140,11 @@ locTest::locTest(ros::NodeHandle& n):
 
     // process
     // from start to end
-    for(int index = startIndex; index<=endIndex; index++)
+    for(int index = indexVector.at(startIndex); index<=indexVector.at(endIndex); index++)
     {
         this->process(index);
 
+        //saving...
         if(index!=startIndex)
         {
             saverTime<<deltaTime<<endl;
@@ -132,6 +154,10 @@ locTest::locTest(ros::NodeHandle& n):
                     <<Ticp(3,0)<<"  "<<Ticp(3,1)<<"  "<<Ticp(3,2)<<"  "<<Ticp(3,3)<<endl;
         }
 
+        // publish
+        velCloudPub.publish(PointMatcher_ros::pointMatcherCloudToRosMsg<float>(velodyneCloud, "robot", ros::Time::now()));
+        mapCloudPub.publish(PointMatcher_ros::pointMatcherCloudToRosMsg<float>(mapCloud, "global", ros::Time::now()));
+        tfBroadcaster.sendTransform(PointMatcher_ros::eigenMatrixToStampedTransform<float>(Ticp, "global", "robot", ros::Time::now()));
     }
 
 }
@@ -149,17 +175,21 @@ void locTest::process(int index)
         return;
     }
 
-    // load velodyne data
     stringstream ss;
-    ss<<index;
+    if(isKITTI)
+        ss<<setw(6)<<setfill('0')<<index;
+    else
+        ss<<setw(10)<<setfill('0')<<index;
+
     string str;
     ss>>str;
-    string veloName = loadVeloDir + str + ".vtk";
-    DP velodyneCloud = DP::load(veloName);
-    inputFilters.apply(velodyneCloud);
+    string veloName = velodyneDirName + str + ".bin";
+    cout<<veloName<<endl;
 
-    cout<<"------------------------------------------------------------------"<<endl;
-    cout<<index<<endl;
+    if(isKITTI)
+        velodyneCloud = this->readKITTIBin(veloName);       // KITTI dataset
+    else
+        velodyneCloud = this->readYQBin(veloName);  // YQ dataset
 
     // icp
 
@@ -179,19 +209,100 @@ void locTest::process(int index)
 
     Tinit = Ticp;
 
-    //check the error
-    gnd(0) = initPoses[index][3];    gnd(1) = initPoses[index][7];
-    loc(0) = Ticp(0, 3);    loc(1) = Ticp(1, 3);
-    double error = this->getLocError(gnd, loc);
-    cout<<"The position is away:    "<<error<<"     meters."<<endl;
-
     // save the results outside this function
 
 }
 
-double locTest::getLocError(Eigen::Vector2d gnd, Eigen::Vector2d loc)
+locTest::DP locTest::readYQBin(string filename)
 {
-    return sqrt(pow(gnd(0)-loc(0),2) + pow(gnd(1)-loc(1),2));
+    DP data;
+
+    fstream input(filename.c_str(), ios::in | ios::binary);
+    if(!input.good()){
+        cerr << "Could not read file: " << filename << endl;
+        exit(EXIT_FAILURE);
+    }
+    input.seekg(0, ios::beg);
+
+    data.addFeature("x",PM::Matrix::Constant(1,300000,0));
+    data.addFeature("y",PM::Matrix::Constant(1,300000,0));
+    data.addFeature("z",PM::Matrix::Constant(1,300000,0));
+    data.addFeature("pad",PM::Matrix::Constant(1,300000,1));
+    data.addDescriptor("intensity",PM::Matrix::Constant(1,300000,0));
+    data.addDescriptor("ring",PM::Matrix::Constant(1,300000,0));
+
+    int i;
+
+    for (i=0; input.good() && !input.eof(); i++) {
+        float a,b;
+        input.read((char *) &a, 4*sizeof(unsigned char));
+        data.features(0,i) = a;
+        input.read((char *) &a, 4*sizeof(unsigned char));
+        data.features(1,i) = a;
+        input.read((char *) &a, 4*sizeof(unsigned char));
+        data.features(2,i) = a;
+        input.read((char *) &b, 4*sizeof(unsigned char));
+        input.read((char *) &a, 4*sizeof(unsigned char));
+        data.descriptors(0,i) = a;
+        input.read((char *) &a, 2*sizeof(unsigned char));
+        data.descriptors(1,i) = a;
+
+        input.read((char *) &b, 10*sizeof(unsigned char));
+
+    }
+    input.close();
+    data.conservativeResize(i);
+
+    return data;
+}
+
+//For kitti dataset
+locTest::DP locTest::readKITTIBin(string fileName)
+{
+    DP tempScan;
+
+    int32_t num = 1000000;
+    float *data = (float*)malloc(num*sizeof(float));
+
+    // pointers
+    float *px = data+0;
+    float *py = data+1;
+    float *pz = data+2;
+    float *pr = data+3;
+
+    // load point cloud
+    FILE *stream;
+    stream = fopen (fileName.c_str(),"rb");
+    num = fread(data,sizeof(float),num,stream)/4;
+
+    //ethz data structure
+    tempScan.addFeature("x", PM::Matrix::Zero(1, num));
+    tempScan.addFeature("y", PM::Matrix::Zero(1, num));
+    tempScan.addFeature("z", PM::Matrix::Zero(1, num));
+    tempScan.addDescriptor("intensity", PM::Matrix::Zero(1, num));
+
+    int x = tempScan.getFeatureStartingRow("x");
+    int y = tempScan.getFeatureStartingRow("y");
+    int z = tempScan.getFeatureStartingRow("z");
+    int intensity = tempScan.getDescriptorStartingRow("intensity");
+
+
+    for (int32_t i=0; i<num; i++)
+    {
+        tempScan.features(x,i) = *px;
+        tempScan.features(y,i) = *py;
+        tempScan.features(z,i) = *pz;
+        tempScan.descriptors(intensity,i) = *pr;
+        px+=4; py+=4; pz+=4; pr+=4;
+    }
+    fclose(stream);
+
+    ///free the ptr
+    {
+        free(data);
+    }
+
+    return tempScan;
 }
 
 
