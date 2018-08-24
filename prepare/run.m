fca2efc4169e8630fe6df4dfaa80245a1952ffd2
Huan Yin <zjuyinhuan@gmail.com>
2018-08-24 20:20:26 +1000

    start loacTest :_:

--- prepare/run.m
+++ prepare/run.m
@@ -1,11 +1,19 @@
 
-icp030001_new = poseSparsification( icp030001, '/home/yh/mapModel/2018/08.03/keep_0301.txt', 0.20);
-getDistribution(icp030001);
-hold on;
-getDistribution(icp030001_new);
+% icp030001_new = poseSparsification( icp030001, '/home/yh/mapModel/2018/08.03/keep_0301.txt', 0.20);
+% getDistribution(icp030001);
+% hold on;
+% getDistribution(icp030001_new);
 
 % [pose10_new, pose10_keep] = ForKITTIPoses( '/home/yh/mapModel/2018/08.02/kitti10_pose.txt', '/home/yh/mapModel/2018/08.02/kitti10_keep.txt', pose10, 0.70);
 % 
 % getDistribution(pose10_new);
 % hold on;
 % getDistribution(pose10_keep);
+
+
+%% test
+
+icp0901_new = poseSparsification( icp0901, '/home/yh/mapModel/2018/08.03/test/keep_0901.txt', 0.20);
+getDistribution(icp0901);
+hold on;
+getDistribution(icp0901_new);
