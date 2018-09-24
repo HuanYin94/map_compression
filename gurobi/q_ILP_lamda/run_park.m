af124b328ef6e4aa4843b34c5f2f29a7a8240693
YH <zjuyinhuan@gmail.com>
2018-09-24 20:38:52 +0800

    finish sth.

--- gurobi/q_ILP_lamda/run_park.m
+++ gurobi/q_ILP_lamda/run_park.m
@@ -1,7 +1,11 @@
-% 0.2
+% 0.05 700
+ 
+% 0.1 1200
+
+% 0.2 2200
 
 totalNum = 447026;
 
-loopCompress(0.1, '/home/yinhuan/compression_exps/park/weightVector.txt', '/home/yinhuan/compression_exps/park/visMatrix/', 50, totalNum, 800, '/home/yinhuan/compression_exps/park/gurobi_compress_0.2/');
+loopCompress(0.1, '/home/yinhuan/compression_exps/park/weightVector.txt', '/home/yinhuan/compression_exps/park/visMatrix/', 50, totalNum, 700, '/home/yinhuan/compression_exps/park/gurobi_compress_0.05/');
 
-ratio = salientNumCnt( '/home/yinhuan/compression_exps/park/gurobi_compress_0.2/', totalNum ) / totalNum
+ratio = salientNumCnt( '/home/yinhuan/compression_exps/park/gurobi_compress_0.05/', totalNum ) / totalNum
