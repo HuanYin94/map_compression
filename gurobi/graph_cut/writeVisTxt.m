c71ca44a1a78212f757e16d246dd757dac826523
Huan Yin <zjuyinhuan@gmail.com>
2018-10-08 20:31:24 +0800

    xxlm

---
+++ gurobi/graph_cut/writeVisTxt.m
@@ -0,0 +1,38 @@
+function [  ] = writeVisTxt( filesDir, saveAddress )
+%WRITEVISTXT Summary of this function goes here
+%   Detailed explanation goes here
+        
+    %  https://github.com/math314/cut-tree 
+    %  roadID mapPointID (as a observe), or Gomory Hu tre cut
+    
+    fileExt = '*.txt';
+    files = dir(fullfile(filesDir,fileExt)); 
+    
+    pathLength= length(files);
+    
+    saveFile = fopen(saveAddress, 'w');
+    
+    for i = 0:pathLength-1
+        i
+        fileCnt = num2str(i);
+        fileName = [filesDir, fileCnt, '.txt'];     
+        file_t = fopen(fileName);
+        
+        pointID = fscanf(file_t, '%d');
+        fclose(file_t);    
+        
+        % from beginning to the end ?
+        pointID = sort(pointID);
+        
+        for m = 1:length(pointID)
+           % write pairs as edges  
+           fprintf(saveFile,'%d ', i);
+           fprintf(saveFile,'%d\n', pointID(m));
+
+        end
+            
+    end
+    fclose(saveFile);
+    
+end
+
