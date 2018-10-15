f4ea8884819781a783e2b9d4d4b3da3592ee1362
YH <zjuyinhuan@gmail.com>
2018-10-15 21:36:23 +0800

    i'm hungry

---
+++ gurobi/graph_cut/d1.m
@@ -0,0 +1,11 @@
+function [ b ] = d1( c )
+%D1 Summary of this function goes here
+%   Detailed explanation goes here
+    
+    % ke yi digui
+
+    disp(c);
+    c= c+1;
+    d1(c);
+end
+
---
+++ gurobi/graph_cut/getPoseGraph.m
@@ -0,0 +1,30 @@
+function [ pointByPose ] = getPoseGraph( filesDir)
+%GETPOSEGRAPH Summary of this function goes here
+%   Detailed explanation goes here
+    
+    fileExt = '*.txt';
+    files = dir(fullfile(filesDir,fileExt)); 
+    
+    pathLength= length(files);
+    
+    pointByPose = [];
+    
+    for i = 0:pathLength-1
+        i
+        
+        fileCnt = num2str(i);
+        fileName = [filesDir, fileCnt, '.txt'];     
+        file_t = fopen(fileName);
+        
+        pointID = fscanf(file_t, '%d');
+        fclose(file_t);    
+        
+        % add this pose to some point        
+        for p = 1:length(pointID)
+           pointByPose(pointID(1), i+1) = i+1; 
+        end
+        
+    end
+
+end
+
