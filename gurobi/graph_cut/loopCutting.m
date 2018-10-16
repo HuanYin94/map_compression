ee906bec2bf132b29fde1552ab10c3ae2d2efacd
Huan Yin <zjuyinhuan@gmail.com>
2018-10-16 16:01:09 +0800

    try networkx to cut the graph

---
+++ gurobi/graph_cut/loopCutting.m
@@ -0,0 +1,7 @@
+function [ output_args ] = loopCutting( input_args )
+%LOOPCUTTING Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ gurobi/graph_cut/python/try.py
@@ -0,0 +1,24 @@
+import sys
+
+import matplotlib.pyplot as plt
+import networkx as nx
+from networkx.algorithms import flow
+
+G = nx.Graph();
+G.add_nodes_from([0,1,2,3,4,5])
+G.add_weighted_edges_from([(0,1,1), (0,2,7), (1,2,1), (1,3,3), (1,4,2), (2,4,4), (3,4,1), (3,5,6), (4,5,2)])
+#nx.set_edge_attributes(G, 99, 'capacity')
+T = nx.gomory_hu_tree(G, capacity='weight')
+                                                                                                                                                                                
+
+#weight = nx.get_edge_attributes(G,'weight')
+#nx.draw_circular(G, with_labels=True, font_weight='bold')
+#layout = nx.circular_layout(G)
+#nx.draw_networkx_edge_labels(G,pos=layout,edge_labels=weight)
+
+weight = nx.get_edge_attributes(T,'weight')
+nx.draw_circular(T, with_labels=True, font_weight='bold')
+layout = nx.circular_layout(T)
+nx.draw_networkx_edge_labels(T,pos=layout,edge_labels=weight)
+
+plt.show()
