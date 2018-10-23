51c25e17b2277f7b77e933f32ec2ddd2274e3011
Huan Yin <zjuyinhuan@gmail.com>
2018-10-23 14:14:03 +0800

    faaaaaaaster

--- gurobi/graph_cut/NetworkX/try.py
+++ gurobi/graph_cut/NetworkX/try.py
@@ -3,12 +3,13 @@ import sys
 import matplotlib.pyplot as plt
 import networkx as nx
 from networkx.algorithms import flow
+from networkx.algorithms.flow import shortest_augmenting_path
 
 G = nx.Graph();
 G.add_nodes_from([0,1,2,3,4,5])
 G.add_weighted_edges_from([(0,1,1), (0,2,7), (1,2,1), (1,3,3), (1,4,2), (2,4,4), (3,4,1), (3,5,6), (4,5,2)])
 #nx.set_edge_attributes(G, 99, 'capacity')
-T = nx.gomory_hu_tree(G, capacity='weight')
+T = nx.gomory_hu_tree(G, 99, flow_func=shortest_augmenting_path)
                                                                                                                                                                                 
 
 #weight = nx.get_edge_attributes(G,'weight')
