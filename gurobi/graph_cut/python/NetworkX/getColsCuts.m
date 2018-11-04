7db9d47e3e3efa69af38c1d20e359505c480a3c0
YH <zjuyinhuan@gmail.com>
2018-11-04 22:50:26 +0800

    rows cut finished, start cols

---
+++ gurobi/graph_cut/python/NetworkX/getColsCuts.m
@@ -0,0 +1,9 @@
+function [  ] = getColsCuts( visMatrixDir, saveFielDir )
+%GETCOLSCUTS Summary of this function goes here
+%   Detailed explanation goes here
+    
+    fileExt = '*.txt';
+    files = dir(fullfile(filesDir,fileExt)); 
+
+end
+
