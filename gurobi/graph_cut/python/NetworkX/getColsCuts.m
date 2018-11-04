6e61d5342b6f9c60083c110cecd2d8c7f0a7d634
YH <zjuyinhuan@gmail.com>
2018-11-04 23:09:58 +0800

    go back sleep

--- gurobi/graph_cut/python/NetworkX/getColsCuts.m
+++ gurobi/graph_cut/python/NetworkX/getColsCuts.m
@@ -1,9 +1,28 @@
-function [  ] = getColsCuts( visMatrixDir, saveFielDir )
+function [  ] = getColsCuts( visMatrixDir, rowsCutDir, saveFileDir )
 %GETCOLSCUTS Summary of this function goes here
 %   Detailed explanation goes here
     
     fileExt = '*.txt';
-    files = dir(fullfile(filesDir,fileExt)); 
+    files_pose = dir(fullfile(rowsCutDir, fileExt));
+    
+    rosCutLength= length(files_pose);
+    
+    for i=1:rosCutLength
+        
+        fileName_pose = [rowsCutDir, num2str(i), '.txt'];     
+        file_pose = fopen(fileName_pose);
+        poseID = fscanf(file_pose, '%d');
+        
+        for j = 1:length(poseID)
+            fileName_point = [visMatrixDir, num2str(poseID(j)), '.txt'];     
+            file_point = fopen(fileName_point);
+            poseID = fscanf(file_pose, '%d');
+    
+            % qiu jiao ji
+            
+        end
+        
+    end
 
 end
 
