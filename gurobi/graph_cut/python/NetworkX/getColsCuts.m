7718fce43ab66a72f91f4d2d08cea077e6cba728
YH <zjuyinhuan@gmail.com>
2018-11-05 09:54:35 +0800

    go for ppt

--- gurobi/graph_cut/python/NetworkX/getColsCuts.m
+++ gurobi/graph_cut/python/NetworkX/getColsCuts.m
@@ -8,19 +8,24 @@ function [  ] = getColsCuts( visMatrixDir, rowsCutDir, saveFileDir )
     rosCutLength= length(files_pose);
     
     for i=1:rosCutLength
-        
+        i
         fileName_pose = [rowsCutDir, num2str(i), '.txt'];     
         file_pose = fopen(fileName_pose);
-        poseID = fscanf(file_pose, '%d');
+        pose_ID = fscanf(file_pose, '%d');
         
-        for j = 1:length(poseID)
-            fileName_point = [visMatrixDir, num2str(poseID(j)), '.txt'];     
+        points_for_section = [];  % all the ovserved Points
+        for j = 1:length(pose_ID)
+            fileName_point = [visMatrixDir, num2str(pose_ID(j)), '.txt'];
             file_point = fopen(fileName_point);
-            poseID = fscanf(file_pose, '%d');
-    
-            % qiu jiao ji
+            point_ID = fscanf(file_pose, '%d');
+            fclose(file_pose);
             
+            % 
+            points_for_section = union(points_for_section, point_ID);
         end
+        
+        fileName_savePoint = [saveFileDir, num2str(i), '.txt'];
+        dlmwrite(fileName_savePoint, points_for_section, 'precision', 10);
         
     end
 
