249e7b7b55161468d3e2637b4aa405309178e1bc
YH <zjuyinhuan@gmail.com>
2018-11-16 19:53:03 +0800

    sparse graph generated

--- gurobi/graph_cut/getPoseGraph_sparse.m
+++ gurobi/graph_cut/getPoseGraph_sparse.m
@@ -1,4 +1,4 @@
-function [  ] = getPoseGraph_sparse( visFilesDir, windowLength, saveAddress,  sparse_separate  )
+function [  ] = getPoseGraph_sparse( visFilesDir, windowLength,  sparse_separate, saveAddress  )
 %GETPOSEGRAPH_SPARSE Summary of this function goes here
 %   Detailed explanation goes here
     
@@ -12,10 +12,43 @@ function [  ] = getPoseGraph_sparse( visFilesDir, windowLength, saveAddress,  sp
     cnt = 1;
     for i=0:sparse_separate:pathLength-1
         node_list(cnt, :) = i;
+        cnt =cnt +1;
     end     
     
     % build the graph from the vis matrix and the sparse nodes
-    visFilesDir
+    % different from the previous one, get the window one by one :)
+    for i=1:length(node_list)
+        
+        % save one by one
+        saveFile = fopen(saveAddress, 'a');
+        
+        % open the first file
+        fileName_i = [visFilesDir, num2str(node_list(i)), '.txt'];     
+        file_t_i = fopen(fileName_i);
+        pointID_i = fscanf(file_t_i, '%d');
+                                                                                                                                                                                                                                            
+        for j=i+1:i+windowLength
+            
+            if j > length(node_list)
+                % do nothing
+            else
+                fileName_j = [visFilesDir, num2str(node_list(j)), '.txt'];     
+                file_t_j= fopen(fileName_j);
+                pointID_j = fscanf(file_t_j, '%d');
+
+                samer = intersect(pointID_i, pointID_j);
+
+                fprintf(saveFile,'%d ', i-1);   % save from zero                     
+                fprintf(saveFile,'%d ', j-1);   % minus one
+                fprintf(saveFile,'%d\n', length(samer));
+            end
+            
+        end
+        
+        fclose all;
+   
+    end
+    
     
     
 end
