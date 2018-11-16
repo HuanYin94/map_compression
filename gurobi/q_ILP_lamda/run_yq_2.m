291878a70299c885d38fa234ef76f1459cbb42c8
YH <zjuyinhuan@gmail.com>
2018-11-16 21:51:48 +0800

    fuck graph cut, time wasted on this useless topic!

--- gurobi/q_ILP_lamda/run_yq_2.m
+++ gurobi/q_ILP_lamda/run_yq_2.m
@@ -1,13 +1,20 @@
 totalNum = 1867290;
 % 
 
-epsilon_soft_100 = Uniform_loopCompress( 0.1, '/home/yinhuan/mapModel/yq/weightVector.txt', '/home/yinhuan/mapModel/yq/visMatrix/', 100, totalNum, 2000, '/home/yinhuan/mapModel/yq/rows_cut/Uniform_compress0.2_100/' );
+% epsilon_soft_100 = Uniform_loopCompress( 0.1, '/home/yinhuan/mapModel/yq/weightVector.txt', '/home/yinhuan/mapModel/yq/visMatrix/', 100, totalNum, 2000, '/home/yinhuan/mapModel/yq/rows_cut/Uniform_compress0.2_100/' );
+% 
+% salientNumCnt('/home/yinhuan/mapModel/yq/rows_cut/Uniform_compress0.2_100/', totalNum );
+% 
+% min_cost_Uniform_100  = get_min_cost('/home/yinhuan/mapModel/yq/rows_cut/DP_compress0.2_100/', '/home/yinhuan/mapModel/yq/weightVector.txt', 0.1, epsilon_soft_100);
 
-salientNumCnt('/home/yinhuan/mapModel/yq/rows_cut/Uniform_compress0.2_100/', totalNum );
 
-min_cost_Uniform_100  = get_min_cost('/home/yinhuan/mapModel/yq/rows_cut/DP_compress0.2_100/', '/home/yinhuan/mapModel/yq/weightVector.txt', 0.1, epsilon_soft_100);
+% sparse test
 
- 
+ epsilon_soft_4_Uni = Uniform_loopCompress( 0.1, '/home/yinhuan/mapModel/yq/weightVector.txt', '/home/yinhuan/mapModel/yq/rows_cut/sparse/visMatrix/', 4, totalNum, 1000, '/home/yinhuan/mapModel/yq/rows_cut/sparse/Uniform_compress0.2_4/' );
+
+salientNumCnt('/home/yinhuan/mapModel/yq/rows_cut/sparse/Uniform_compress0.2_4/', totalNum );
+
+min_cost_Uniform_4  = get_min_cost('/home/yinhuan/mapModel/yq/rows_cut/sparse/Uniform_compress0.2_4/', '/home/yinhuan/mapModel/yq/weightVector.txt', 0.1, epsilon_soft_4_Uni);
  
 
 
