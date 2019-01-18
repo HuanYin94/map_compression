efc574ccc17fc5fabadb026ac1525af5a81cf6c5
YH <zjuyinhuan@gmail.com>
2019-01-18 14:28:11 +0800

    I must finish the exps today

---
+++ gurobi/q_ILP_lamda/iter_run/rename.m
@@ -0,0 +1,9 @@
+totalNum_0_500 = totalNum_0;
+totalNum_1_500 = totalNum_1;
+totalNum_2_500 = totalNum_2;
+totalNum_3_500 = totalNum_3;
+totalNum_4_500 = totalNum_4;
+totalNum_5_500 = totalNum_5;
+totalNum_6_500 = totalNum_6;
+totalNum_7_500 = totalNum_7;
+
