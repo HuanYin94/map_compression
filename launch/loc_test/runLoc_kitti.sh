babcf23ac6e3ff5b07ff071f66cf62c7f20690c9
Huan Yin <zjuyinhuan@gmail.com>
2019-02-13 23:03:11 +0300

    ...

---
+++ launch/loc_test/runLoc_kitti.sh
@@ -0,0 +1,12 @@
+roslaunch map_compression locTest_kitti.launch wholeMapName:="/home/yh/mapModel/2019/01.09/locTest/Learned_20.ply" savePoseName:="/home/yh/mapModel/2019/01.09/locTest/result/Learned_20_pose.txt" saveTimeName:="/home/yh/mapModel/2019/01.09/locTest/result/Learned_20_time.txt" saveIterName:="/home/yh/mapModel/2019/01.09/locTest/result/Learned_20_iter.txt"
+
+
+<<COMMENT
+roslaunch map_compression locTest_kitti.launch wholeMapName:="/home/yh/mapModel/2019/01.09/locTest/Learned_kitti_20_compressed.ply" savePoseName:="/home/yh/mapModel/2019/01.09/locTest/result/Learned_20_pose.txt" saveTimeName:="/home/yh/mapModel/2019/01.09/locTest/result/Learned_20_time.txt" saveIterName:="/home/yh/mapModel/2019/01.09/locTest/result/Learned_20_iter.txt"
+
+roslaunch map_compression locTest_kitti.launch wholeMapName:="/home/yh/mapModel/2019/01.09/locTest/Learned_kitti_20_compressed.ply" savePoseName:="/home/yh/mapModel/2019/01.09/locTest/result/Learned_20_pose.txt" saveTimeName:="/home/yh/mapModel/2019/01.09/locTest/result/Learned_20_time.txt" saveIterName:="/home/yh/mapModel/2019/01.09/locTest/result/Learned_20_iter.txt"
+
+roslaunch map_compression locTest_kitti.launch wholeMapName:="/home/yh/mapModel/2019/01.09/locTest/Learned_kitti_20_compressed.ply" savePoseName:="/home/yh/mapModel/2019/01.09/locTest/result/Learned_20_pose.txt" saveTimeName:="/home/yh/mapModel/2019/01.09/locTest/result/Learned_20_time.txt" saveIterName:="/home/yh/
+mapModel/2019/01.09/locTest/result/Learned_20_iter.txt"
+
+COMMENT
