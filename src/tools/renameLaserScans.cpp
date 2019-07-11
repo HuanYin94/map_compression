71db68a39f4027f1c49570b54689361d7655dcf4
Huan Yin <zjuyinhuan@gmail.com>
2019-07-11 18:47:02 +0800

    transfer

--- src/tools/renameLaserScans.cpp
+++ src/tools/renameLaserScans.cpp
@@ -40,8 +40,10 @@ public:
     string saveScanDir;
     string loadScanDir;
 
-    void process(int cnt);
+    vector<int> indexVector;
 
+    void process(int cnt);
+    void filecopy(FILE *dest, FILE *src);
 };
 
 renameLaserScans::~renameLaserScans()
@@ -56,7 +58,6 @@ renameLaserScans::renameLaserScans(ros::NodeHandle& n):
     saveScanDir(getParam<string>("saveScanDir", "."))
 {
 
-    vector<int> indexVector;
     int temp;
     ifstream in(indexFileName);
     for(int y=0; y<999999; y++)
@@ -67,9 +68,13 @@ renameLaserScans::renameLaserScans(ros::NodeHandle& n):
     }
     in.close();
 
-    for(int i=0; i<indexVector.size(); i++)
-    {
+    int totalCnt = indexVector.size()-1;
 
+    for(int cnt=0; cnt<totalCnt; cnt++)
+    {
+        this->process(cnt);
+
+        cout<<cnt<<"    "<<totalCnt<<endl;
     }
 
 }
@@ -77,6 +82,75 @@ renameLaserScans::renameLaserScans(ros::NodeHandle& n):
 void renameLaserScans::process(int cnt)
 {
 
+    int oldIndex = indexVector.at(cnt);
+    int newIndex = cnt;
+
+    string veloNameOld, veloNameNew;
+
+    if(!isChery)
+    {
+
+        stringstream newSs, oldSs;
+        if(isKITTI)
+        {
+            oldSs<<setw(6)<<setfill('0')<<oldIndex;
+            newSs<<setw(6)<<setfill('0')<<newIndex;
+        }
+        else
+        {
+            oldSs<<setw(10)<<setfill('0')<<oldIndex;
+            newSs<<setw(10)<<setfill('0')<<newIndex;
+        }
+
+        string oldStr, newStr;
+        oldSs >> oldStr;
+        newSs >> newStr;
+
+        veloNameOld = loadScanDir + oldStr + ".bin";
+        veloNameNew = saveScanDir + newStr + ".bin";
+    }
+    else
+    {
+        veloNameOld = loadScanDir + std::to_string(oldIndex) + ".vtk";
+        veloNameNew = saveScanDir + std::to_string(newIndex) + ".vtk";
+    }
+
+//    int result = rename( veloNameOld.c_str() , veloNameNew.c_str() );
+//    if ( result == 0 )
+//    {
+//        puts ( "File successfully renamed  ");
+//    }
+//    else
+//    {
+//        cout<<veloNameOld<<endl;
+//        cout<<veloNameNew<<endl;
+//        perror( "Error renaming file  ");
+//    }
+
+    std::ifstream srce( veloNameOld.c_str(), std::ios::binary ) ;
+    std::ofstream dest( veloNameNew.c_str(), std::ios::binary ) ;
+    dest << srce.rdbuf() ;
+
+//    FILE * infile  = fopen(veloNameOld.c_str(),  "rb");
+//    FILE * outfile = fopen(veloNameNew.c_str(), "wb");
+//    filecopy(outfile, infile);
+//    fclose(infile);
+//    fclose(outfile);
+
+}
+
+void renameLaserScans::filecopy(FILE *dest, FILE *src)
+{
+    char ch;
+    rewind(src);
+    rewind(dest);
+    while((ch=fgetc(src)) != EOF)
+    {
+        fputc(ch, dest);
+    }
+    fflush(dest);
+    rewind(src);
+    rewind(dest);
 }
 
 int main(int argc, char **argv)
