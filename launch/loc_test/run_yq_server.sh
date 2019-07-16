90a9b54a2f81362cc210bfa2bd38c56657faae3b
YH <zjuyinhuan@gmail.com>
2019-07-16 22:03:25 +0800

    loc_kitti finished

---
+++ launch/loc_test/run_yq_server.sh
@@ -0,0 +1,142 @@
+
+:<<BLOCK
+
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b4000_ILP.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b4000_ILP.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b4000_learn.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b4000_learn.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b4000_voxel.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b4000_voxel.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b4000_cluster.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b4000_cluster.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b4000_observe.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b4000_observe.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b4000_saliency.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b4000_saliency.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+
+
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b4000_random_1.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b4000_random_1.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b4000_random_2.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b4000_random_2.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b4000_random_3.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b4000_random_3.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b4000_random_4.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b4000_random_4.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b4000_random_5.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b4000_random_5.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+
+
+
+
+
+
+
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b3000_ILP.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b3000_ILP.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b3000_learn.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b3000_learn.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b3000_voxel.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b3000_voxel.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b3000_cluster.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b3000_cluster.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b3000_observe.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b3000_observe.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b3000_saliency.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b3000_saliency.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b3000_random_1.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b3000_random_1.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b3000_random_2.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b3000_random_2.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b3000_random_3.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b3000_random_3.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b3000_random_4.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b3000_random_4.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b3000_random_5.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b3000_random_5.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b2000_ILP.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b2000_ILP.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b2000_learn.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b2000_learn.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b2000_voxel.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b2000_voxel.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b2000_cluster.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b2000_cluster.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b2000_observe.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b2000_observe.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b2000_saliency.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b2000_saliency.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+
+
+
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b2000_random_1.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b2000_random_1.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b2000_random_2.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b2000_random_2.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b2000_random_3.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b2000_random_3.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b2000_random_4.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b2000_random_4.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b2000_random_5.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b2000_random_5.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+
+
+
+
+
+
+
+
+BLOCK
+
+
+
+
+
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b1000_ILP.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b1000_ILP.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b1000_learn.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b1000_learn.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b1000_voxel.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b1000_voxel.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b1000_cluster.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b1000_cluster.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b1000_observe.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b1000_observe.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b1000_saliency.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b1000_saliency.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b1000_random_1.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b1000_random_1.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b1000_random_2.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b1000_random_2.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b1000_random_3.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b1000_random_3.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b1000_random_4.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b1000_random_4.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+roslaunch map_compression locTest_yq_server.launch wholeMapName:="/home/yinhuan/Data/mapModel/locTest/yq/loc_yq_b1000_random_5.ply" savePoseName:="/home/yinhuan/Data/mapModel/locTest/yq_pose/pose_yq_b1000_random_5.txt" saveTimeName:="/home/yinhuan/Data/mapModel/locTest/time.txt" saveIterName:="/home/yinhuan/Data/mapModel/locTest/iter.txt"
+
+
+
