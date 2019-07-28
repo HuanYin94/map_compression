19f5ca5076616e525266e8f4b451089bfc1c0cb0
Huan Yin <zjuyinhuan@gmail.com>
2019-07-28 21:59:34 +0800

    ooo

--- launch/comparisons/runRandom.sh
+++ launch/comparisons/runRandom.sh
@@ -35,44 +35,32 @@ roslaunch map_compression randomSampleMap.launch keepPointsNum:="406702" loadMap
 
 
 
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="21970" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b1000_random_1.ply"
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="21970" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b1000_random_2.ply"
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="21970" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b1000_random_3.ply"
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="21970" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b1000_random_4.ply"
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="21970" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b1000_random_5.ply"
+roslaunch map_compression randomSampleMap.launch keepPointsNum:="22275" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b1000_random_1.ply"
+roslaunch map_compression randomSampleMap.launch keepPointsNum:="22275" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b1000_random_2.ply"
+roslaunch map_compression randomSampleMap.launch keepPointsNum:="22275" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b1000_random_3.ply"
+roslaunch map_compression randomSampleMap.launch keepPointsNum:="22275" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b1000_random_4.ply"
+roslaunch map_compression randomSampleMap.launch keepPointsNum:="22275" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b1000_random_5.ply"
 
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="46177" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b2000_random_1.ply"
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="46177" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b2000_random_2.ply"
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="46177" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b2000_random_3.ply"
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="46177" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b2000_random_4.ply"
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="46177" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b2000_random_5.ply"
-
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="71320" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b3000_random_1.ply"
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="71320" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b3000_random_2.ply"
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="71320" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b3000_random_3.ply"
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="71320" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b3000_random_4.ply"
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="71320" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b3000_random_5.ply"
-
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="96993" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b4000_random_1.ply"
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="96993" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b4000_random_2.ply"
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="96993" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b4000_random_3.ply"
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="96993" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b4000_random_4.ply"
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="96993" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b4000_random_5.ply"
+roslaunch map_compression randomSampleMap.launch keepPointsNum:="46825" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b2000_random_1.ply"
+roslaunch map_compression randomSampleMap.launch keepPointsNum:="46825" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b2000_random_2.ply"
+roslaunch map_compression randomSampleMap.launch keepPointsNum:="46825" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b2000_random_3.ply"
+roslaunch map_compression randomSampleMap.launch keepPointsNum:="46825" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b2000_random_4.ply"
+roslaunch map_compression randomSampleMap.launch keepPointsNum:="46825" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b2000_random_5.ply"
 
 BLOCK
 
+roslaunch map_compression randomSampleMap.launch keepPointsNum:="72548" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b3000_random_1.ply"
+roslaunch map_compression randomSampleMap.launch keepPointsNum:="72548" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b3000_random_2.ply"
+roslaunch map_compression randomSampleMap.launch keepPointsNum:="72548" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b3000_random_3.ply"
+roslaunch map_compression randomSampleMap.launch keepPointsNum:="72548" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b3000_random_4.ply"
+roslaunch map_compression randomSampleMap.launch keepPointsNum:="72548" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b3000_random_5.ply"
 
+roslaunch map_compression randomSampleMap.launch keepPointsNum:="99213" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b4000_random_1.ply"
+roslaunch map_compression randomSampleMap.launch keepPointsNum:="99213" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b4000_random_2.ply"
+roslaunch map_compression randomSampleMap.launch keepPointsNum:="99213" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b4000_random_3.ply"
+roslaunch map_compression randomSampleMap.launch keepPointsNum:="99213" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b4000_random_4.ply"
+roslaunch map_compression randomSampleMap.launch keepPointsNum:="99213" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b4000_random_5.ply"
 
-
-
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="169305" loadMapName:="/home/yh/mapModel/2019/07.22/wholeMap_yq.ply"  saveMapName:="/home/yh/mapModel/2019/07.22/loc/loc_yq_b2000_random_5.ply"
-
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="406702" loadMapName:="/home/yh/mapModel/2019/07.22/wholeMap_yq.ply"  saveMapName:="/home/yh/mapModel/2019/07.22/loc/loc_yq_b4000_random_5.ply"
-
-
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="46177" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b2000_random_5.ply"
-
-roslaunch map_compression randomSampleMap.launch keepPointsNum:="96993" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply"  saveMapName:="/home/yh/mapModel/2019/07.21/loc/loc_kitti_b4000_random_5.ply"
 
 
 
--- launch/learn_program/runLoad_201907.sh
+++ launch/learn_program/runLoad_201907.sh
@@ -1,3 +1,6 @@
+
+:<<BLOCK
+
 roslaunch map_compression loadProResult.launch loadCompressIndex:="/home/yh/mapModel/2019/07.22/program/compressIndex_final_b4000.txt" loadMapName:="/home/yh/mapModel/2019/07.22/wholeMap_yq.ply" saveCloudName:="/home/yh/mapModel/2019/07.22/program/wholeMap_yq_b4000.ply"
 
 roslaunch map_compression loadProResult.launch loadCompressIndex:="/home/yh/mapModel/2019/07.22/program/compressIndex_final_b3000.txt" loadMapName:="/home/yh/mapModel/2019/07.22/wholeMap_yq.ply" saveCloudName:="/home/yh/mapModel/2019/07.22/program/wholeMap_yq_b3000.ply"
@@ -6,3 +9,13 @@ roslaunch map_compression loadProResult.launch loadCompressIndex:="/home/yh/mapM
 
 roslaunch map_compression loadProResult.launch loadCompressIndex:="/home/yh/mapModel/2019/07.22/program/compressIndex_final_b1000.txt" loadMapName:="/home/yh/mapModel/2019/07.22/wholeMap_yq.ply" saveCloudName:="/home/yh/mapModel/2019/07.22/program/wholeMap_yq_b1000.ply"
 
+
+BLOCK
+
+roslaunch map_compression loadProResult.launch loadCompressIndex:="/home/yh/mapModel/2019/07.21/program/compressIndex_final_b4000.txt" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply" saveCloudName:="/home/yh/mapModel/2019/07.21/program/wholeMap_kitti_b4000.ply"
+
+roslaunch map_compression loadProResult.launch loadCompressIndex:="/home/yh/mapModel/2019/07.21/program/compressIndex_final_b3000.txt" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply" saveCloudName:="/home/yh/mapModel/2019/07.21/program/wholeMap_kitti_b3000.ply"
+
+roslaunch map_compression loadProResult.launch loadCompressIndex:="/home/yh/mapModel/2019/07.21/program/compressIndex_final_b2000.txt" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply" saveCloudName:="/home/yh/mapModel/2019/07.21/program/wholeMap_kitti_b2000.ply"
+
+roslaunch map_compression loadProResult.launch loadCompressIndex:="/home/yh/mapModel/2019/07.21/program/compressIndex_final_b1000.txt" loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply" saveCloudName:="/home/yh/mapModel/2019/07.21/program/wholeMap_kitti_b1000.ply"
