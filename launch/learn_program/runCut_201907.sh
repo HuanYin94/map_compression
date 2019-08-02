f24fa2446e32960e73e94e73a70405ca32cf6a7d
Huan Yin <zjuyinhuan@gmail.com>
2019-08-02 15:36:06 +0800

    111

--- launch/learn_program/runCut_201907.sh
+++ launch/learn_program/runCut_201907.sh
@@ -10,10 +10,13 @@
 
 
 
-roslaunch map_compression mapCutterTraj.launch loadMapName:="/home/yh/mapModel/2019/07.21/program/wholeMap_kitti_b4000.ply" saveTrainName:="/home/yh/mapModel/2019/07.21/learn/train_kitti_b4000.ply" saveTestName:="/home/yh/mapModel/2019/07.21/learn/test_kitti_b4000.ply"
+#roslaunch map_compression mapCutterTraj.launch loadMapName:="/home/yh/mapModel/2019/07.21/program/wholeMap_kitti_b4000.ply" saveTrainName:="/home/yh/mapModel/2019/07.21/learn/train_kitti_b4000.ply" saveTestName:="/home/yh/mapModel/2019/07.21/learn/test_kitti_b4000.ply"
 
-roslaunch map_compression mapCutterTraj.launch loadMapName:="/home/yh/mapModel/2019/07.21/program/wholeMap_kitti_b3000.ply" saveTrainName:="/home/yh/mapModel/2019/07.21/learn/train_kitti_b3000.ply" saveTestName:="/home/yh/mapModel/2019/07.21/learn/test_kitti_b3000.ply"
+#roslaunch map_compression mapCutterTraj.launch loadMapName:="/home/yh/mapModel/2019/07.21/program/wholeMap_kitti_b3000.ply" saveTrainName:="/home/yh/mapModel/2019/07.21/learn/train_kitti_b3000.ply" saveTestName:="/home/yh/mapModel/2019/07.21/learn/test_kitti_b3000.ply"
 
 #roslaunch map_compression mapCutterTraj.launch loadMapName:="/home/yh/mapModel/2019/07.21/program/wholeMap_kitti_b2000.ply" saveTrainName:="/home/yh/mapModel/2019/07.21/learn/train_kitti_b2000.ply" saveTestName:="/home/yh/mapModel/2019/07.21/learn/test_kitti_b2000.ply"
 
 #roslaunch map_compression mapCutterTraj.launch loadMapName:="/home/yh/mapModel/2019/07.21/program/wholeMap_kitti_b1000.ply" saveTrainName:="/home/yh/mapModel/2019/07.21/learn/train_kitti_b1000.ply" saveTestName:="/home/yh/mapModel/2019/07.21/learn/test_kitti_b1000.ply"
+
+
+roslaunch map_compression mapCutterTraj.launch loadMapName:="/home/yh/mapModel/2019/07.21/wholeMap_kitti.ply" saveTrainName:="/home/yh/no.ply" saveTestName:="/home/yh/no.ply"
